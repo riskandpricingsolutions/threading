3a376f31e61f2d464223149273d0da37118cab74
kenneth wilson <kennysquaredonline@gmail.com>
2017-11-18 23:29:37 +0000

    Added a simple SynchronizationContext

---
+++ Threading/SingleThreadedSynchronizationContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RiskAndPricingSolutions.Threading
+{
+    /// <summary>
+    /// A simple implementation of a SynchronizationContext for educational purposes.
+    /// </summary>
+    public class SingleThreadedSynchronizationContext : SynchronizationContext
+    {
+        public SingleThreadedSynchronizationContext()
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        _callbacks.Take()();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }) {Name = "SingleThreadedSynchronizationContext" }
+            .Start();
+        }
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            _callbacks.Add(() => d(state));
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            var tcs = new TaskCompletionSource<object>();
+
+            _callbacks.Add(() =>
+            {
+                d(state);
+                tcs.SetResult(null);
+            });
+
+            tcs.Task.Wait();
+        }
+
+        private readonly BlockingCollection<Action> _callbacks = 
+            new BlockingCollection<Action>();
+    }
+}
---
+++ UnitTests/SingleThreadedSynchronizationContextTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace RiskAndPricingSolutions.Threading.UnitTests
+{
+    [TestFixture]
+    public class SingleThreadedSynchronizationContextTest
+    {
+        [Test]
+        public void TestPost()
+        {
+            var postComplete = false;
+            var sync = new SingleThreadedSynchronizationContext();
+            var latch = new ManualResetEvent(false);
+
+            sync.Post(state =>
+            {
+                postComplete = true;
+                latch.Set();
+            }, null);
+
+            Assert.IsFalse(postComplete);
+            latch.WaitOne();
+            Assert.IsTrue(postComplete);
+        }
+
+        [Test]
+        public void TesSend()
+        {
+            var sendComplete = false;
+            var sync = new SingleThreadedSynchronizationContext();
+   
+            sync.Send(state =>
+            {
+                sendComplete = true;
+            }, null);
+
+            Assert.IsTrue(sendComplete);
+        }
+    }
+}
